011c4a31ea453968ff2db2cd854d624924341984
askatsevalov <79154721620@yandex.ru>
2020-04-14 23:07:03 +0300

    Mafia roles and instructions

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -1,7 +1,11 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Audio;
+using Discord.Commands;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +36,7 @@ namespace OrtemDiscordBot
 
             int argPos = 0;
 
-            if (!(message.HasCharPrefix('$', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos)) || message.Author.IsBot)
+            if (!(message.HasCharPrefix('$', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos)))
                 return;
 
             var context = new SocketCommandContext(client, message);
@@ -40,7 +44,7 @@ namespace OrtemDiscordBot
             await commands.ExecuteAsync(context, argPos, null);
         }
     }
-
+    
 	public class InfoModule : ModuleBase<SocketCommandContext>
 	{
 		[Command("say")]
@@ -54,6 +58,171 @@ namespace OrtemDiscordBot
         {
             var userInfo = user ?? Context.Client.CurrentUser;
             await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}\nСтатус: {userInfo.Status}\n{(userInfo.Activity != null ? $"Играет в {userInfo.Activity}" : "Не занят")}");
+        }
+    }
+
+    public class VoiceModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("salam", RunMode = RunMode.Async)]
+        [Summary("Salams user when he connects to voice channel.")]
+        public async Task SalamAsync([Summary("Discriminator of the user")]string discr)
+        {
+            SocketVoiceChannel ch = Program.govor;
+            IAudioClient audioClient = await ch.ConnectAsync();
+
+            Process pr = Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"audio/salam_{discr}.mp3\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+
+            using (var ffmpeg = pr)
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = audioClient.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
+            await ch.DisconnectAsync();
+        }
+
+        [Command("leave", RunMode = RunMode.Async)]
+        [Summary("Says goodbye to user when he disconnects from voice channel.")]
+        public async Task LeaveAsync()
+        {
+            SocketVoiceChannel ch = Program.govor;
+            IAudioClient audioClient = await ch.ConnectAsync();
+
+            Process pr = Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"audio/leave.mp3\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+
+            using (var ffmpeg = pr)
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = audioClient.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
+            await ch.DisconnectAsync();
+        }
+    }
+
+    [Group("mafia")]
+    public class MafiaModule : ModuleBase<SocketCommandContext>
+    {
+        Random rnd = new Random();
+
+        List<SocketUser> players; //List of active (and alive) players
+        string CurrentStage; //Current stage of the game
+
+        //Roles
+        List<IUser> MafiaPlayers = new List<IUser>();
+        List<IUser> CitizenPlayers = new List<IUser>();
+        IUser DoctorPlayer;
+        IUser PolicemanPlayer;
+        IUser WhorePlayer;
+        //=====
+
+        //Instructions
+        string MafiaInstruction = @"Ты Мафия!\n
+                                    Инструкция:
+                                        1. Все команды писать в этот чат.
+                                        2. Когда будет объявлен раунд 'Убийство', введи команду $mafia kill *Дискриминатор игрока, которого нужно убить* (например, $mafia kill 9929). (Не забудь посоветоваться с напарником, если он есть).
+                                        3. Остальные личные подсказки и инcтрукции будут показаны в этом чате.";
+        string CitizenInstruction = @"Ты Мирный житель!";
+        string PolicemanInstruction = @"Ты Полицейский!\n
+                                    Инструкция:
+                                        1. Все команды писать в этот чат.
+                                        2. Когда будет объявлен раунд 'Расследование', введи команду $mafia reveal *Дискриминатор игрока, которого нужно раскрыть* (например, $mafia reveal 9929).
+                                        3. Остальные личные подсказки и инcтрукции будут показаны в этом чате.";
+        string DoctorInstruction = @"Ты Доктор!\n
+                                    Инструкция:
+                                        1. Все команды писать в этот чат.
+                                        2. Когда будет объявлен раунд 'Лечение', введи команду $mafia heel *Дискриминатор игрока, которого нужно вылечить* (например, $mafia heel 9929).
+                                        3. Остальные личные подсказки и инcтрукции будут показаны в этом чате.";
+        string WhoreInstruction = @"Ты Куртизанка!\n
+                                    Инструкция:
+                                        1. Все команды писать в этот чат.
+                                        2. Когда будет объявлен раунд 'Изнасилование', введи команду $mafia fuck *Дискриминатор игрока, которого нужно изнасиловать* (например, $mafia fuck 9929).
+                                        3. Остальные личные подсказки и инcтрукции будут показаны в этом чате.";
+        //============
+
+        [Command("play")]
+        [Summary("Begins new Mafia game.")]
+        public async Task MafiaPlayAsync() {
+            if (Context.Channel != Program.mafia) return;
+            //Get active players
+            players = new List<SocketUser>(Program.govor.Users);
+            players.RemoveAll(x => x.IsBot);
+            //==================
+
+            //Give every player a role
+            List<int> listNumbers = new List<int>();
+            listNumbers.AddRange(Enumerable.Range(0, players.Count).OrderBy(i => rnd.Next()));
+            if (players.Count < 4) await Program.test.SendMessageAsync("Количество игроков должно быть 4 и больше.");
+            else if (players.Count == 4)
+            {
+                MafiaPlayers.Add(players[listNumbers[0]]);
+                for (int i = 1; i < players.Count; i++) CitizenPlayers.Add(players[listNumbers[i]]);
+            }
+            else if (players.Count == 5)
+            {
+                MafiaPlayers.Add(players[listNumbers[0]]);
+                PolicemanPlayer = players[listNumbers[1]];
+                for (int i = 2; i < players.Count; i++) CitizenPlayers.Add(players[listNumbers[i]]);
+            }
+            else if (players.Count == 6)
+            {
+                MafiaPlayers.Add(players[listNumbers[0]]);
+                MafiaPlayers.Add(players[listNumbers[1]]);
+                PolicemanPlayer = players[listNumbers[2]];
+                for (int i = 3; i < players.Count; i++) CitizenPlayers.Add(players[listNumbers[i]]);
+            }
+            else if (players.Count == 7)
+            {
+                MafiaPlayers.Add(players[listNumbers[0]]);
+                MafiaPlayers.Add(players[listNumbers[1]]);
+                PolicemanPlayer = players[listNumbers[2]];
+                DoctorPlayer = players[listNumbers[3]];
+                for (int i = 4; i < players.Count; i++) CitizenPlayers.Add(players[listNumbers[i]]);
+            }
+            else if (players.Count == 8)
+            {
+                MafiaPlayers.Add(players[listNumbers[0]]);
+                MafiaPlayers.Add(players[listNumbers[1]]);
+                PolicemanPlayer = players[listNumbers[2]];
+                DoctorPlayer = players[listNumbers[3]];
+                WhorePlayer = players[listNumbers[4]];
+                for (int i = 5; i < players.Count; i++) CitizenPlayers.Add(players[listNumbers[i]]);
+            }
+            //========================
+
+            //Send every player his role's instruction
+            foreach (IUser m in MafiaPlayers) await m.SendMessageAsync(MafiaInstruction);
+            if (MafiaPlayers.Count > 1)
+            {
+                await MafiaPlayers[0].SendMessageAsync($"Твой напарник - {MafiaPlayers[1].Username}#{MafiaPlayers[1].Discriminator}");
+                await MafiaPlayers[1].SendMessageAsync($"Твой напарник - {MafiaPlayers[0].Username}#{MafiaPlayers[0].Discriminator}");
+            }
+            foreach (IUser c in CitizenPlayers) await c.SendMessageAsync(CitizenInstruction);
+            if (PolicemanPlayer != null) await PolicemanPlayer?.SendMessageAsync(PolicemanInstruction);
+            if (DoctorPlayer != null) await DoctorPlayer?.SendMessageAsync(DoctorInstruction);
+            if (WhorePlayer != null) await WhorePlayer?.SendMessageAsync(WhoreInstruction);
+            //========================================
+
+            //Let's begin
+            CurrentStage = "Начало";
+            StringBuilder str_players = new StringBuilder("Начинаем игру Мафия. Список игроков:\n");
+            foreach (SocketUser p in players) str_players.Append($"{p.Username}\n");
+            await Program.mafia.SendMessageAsync(str_players.ToString());
+            //===========
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,16 +5,35 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Collections.Specialized;
 using Discord.Commands;
+using System.Diagnostics;
+using Discord.Audio;
 
 namespace OrtemDiscordBot
 {
     class Program
     {
         NameValueCollection config = ConfigurationManager.AppSettings;
+
         private DiscordSocketClient client;
         private CommandService cmdService;
         private CommandHandler cmdHandler;
         private LogService logService;
+
+        internal static SocketGuild guild8;
+
+        internal static SocketVoiceChannel govor;
+        internal static SocketTextChannel pisal;
+        internal static SocketTextChannel test;
+        internal static SocketTextChannel mafia;
+
+        internal static SocketUser Ruslan;
+        internal static SocketUser Zafar;
+        internal static SocketUser Andrey;
+        internal static SocketUser Vadim;
+        internal static SocketUser Ortem;
+        internal static SocketUser Lera;
+        internal static SocketUser Lera2;
+        internal static SocketUser Nekit;
 
         public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
@@ -26,20 +45,65 @@ namespace OrtemDiscordBot
             logService = new LogService(client, cmdService);
             cmdHandler = new CommandHandler(client, cmdService);
             await cmdHandler.InstallCommandsAsync();
-            
+
             await client.LoginAsync(TokenType.Bot, config.Get("token"));
             await client.StartAsync();
             //====================
 
-            //Add event handlers
-            client.Log += (LogMessage msg) =>
+            client.Ready += () =>
             {
-                Console.WriteLine(msg);
+                guild8 = client.GetGuild(Convert.ToUInt64(config.Get("guild8_id")));
+
+                govor = client.GetChannel(Convert.ToUInt64(config.Get("govor_id"))) as SocketVoiceChannel;
+                pisal = client.GetChannel(Convert.ToUInt64(config.Get("pisal_id"))) as SocketTextChannel;
+                test = client.GetChannel(Convert.ToUInt64(config.Get("test_id"))) as SocketTextChannel;
+                mafia = client.GetChannel(Convert.ToUInt64(config.Get("mafia_id"))) as SocketTextChannel;
+
+                Ruslan = client.GetUser(Convert.ToUInt64(config.Get("ruslan_id")));
+                Zafar = client.GetUser(Convert.ToUInt64(config.Get("zafar_id")));
+                Andrey = client.GetUser(Convert.ToUInt64(config.Get("andrey_id")));
+                Vadim = client.GetUser(Convert.ToUInt64(config.Get("vadim_id")));
+                Ortem = client.GetUser(Convert.ToUInt64(config.Get("ortem_id")));
+                Lera = client.GetUser(Convert.ToUInt64(config.Get("lera_id")));
+                Lera2 = client.GetUser(Convert.ToUInt64(config.Get("lera2_id")));
+                Nekit = client.GetUser(Convert.ToUInt64(config.Get("nekit_id")));
+
                 return Task.CompletedTask;
             };
-            //==================
+
+            client.UserVoiceStateUpdated += async (SocketUser user, SocketVoiceState before, SocketVoiceState after) =>
+            {
+                if (user.IsBot) return;
+                if (govor == after.VoiceChannel)
+                    await test.SendMessageAsync($"$salam {user.Discriminator}");
+                else if (govor == before.VoiceChannel)
+                    await test.SendMessageAsync($"$leave");
+            };
+            client.MessageReceived += async (SocketMessage msg) =>
+            {
+                if (msg.Author.IsBot && msg.Content.StartsWith('$')) await msg.DeleteAsync();
+            };
 
             await Task.Delay(-1);
+        }
+
+        private Process CreateStream(string path) => Process.Start(new ProcessStartInfo
+        {
+            FileName = "ffmpeg",
+            Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        });
+
+        private async Task PlaySoundAsync(IAudioClient client, string path)
+        {
+            using (var ffmpeg = CreateStream(path))
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
         }
     }
 }
